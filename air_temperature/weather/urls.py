8692d789b64df5ba697e9d37fd456d275160e048
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-04-09 21:35:45 +0100

    Implemented WeatherForm and Integrated API with dynamic user input

--- air_temperature/weather/urls.py
+++ air_temperature/weather/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home_page'),
-    path('api/', views.weather_api, name='weather_api')
+    path('weather/', views.weather_api, name='weather_api')
 ]
--- air_temperature/weather/views.py
+++ air_temperature/weather/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .forms import WeatherForm
 from django.conf import settings
 import requests
 import logging
@@ -26,44 +27,50 @@ def valiidate_zip_code(zip_code):
     return False
 
 def weather_api(request):
-    # getting inputs
-    city = request.GET.get('city')
-    lat = request.GET.get('lat')
-    lon = request.GET.get('lon')
-    zip_code = request.GET.get('zip')
+    if request.method == 'GET':
+        form = WeatherForm(request.GET or None)
+        if form.is_valid():
+            # getting inputs
+            city = request.GET.get('city')
+            lat = request.GET.get('lat')
+            lon = request.GET.get('lon')
+            zip_code = request.GET.get('zip')
 
-    # validate inputs
-    if lat and lon:
-        if not validate_lat_lon(lat, lon):
-            return JsonResponse({'error': 'Invlaid latitude or longitude'},status=400)
-    elif zip_code:
-        if not valiidate_zip_code(zip_code):
-            return JsonResponse({'error': 'Invalid ZIP code format. correct format Ex. "12345,us".'}, status=400)
+            # validate inputs
+            if lat and lon:
+                if not validate_lat_lon(lat, lon):
+                    return JsonResponse({'error': 'Invlaid latitude or longitude'},status=400)
+            elif zip_code:
+                if not valiidate_zip_code(zip_code):
+                    return JsonResponse({'error': 'Invalid ZIP code format. correct format Ex. "12345,us".'}, status=400)
 
-    # Construct the API based on the input type
-    if city:
-        url = f'{base_url}?q={city}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
-    elif lat and lon:
-        url = f'{base_url}?lat={lat}&lon={lon}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
-    elif zip_code:
-        url = f'{base_url}?zip={zip_code}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
+            # Construct the API based on the input type
+            if city:
+                url = f'{base_url}?q={city}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
+            elif lat and lon:
+                url = f'{base_url}?lat={lat}&lon={lon}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
+            elif zip_code:
+                url = f'{base_url}?zip={zip_code}&appid={settings.OPENWEATHER_API_KEY}&units=metric'
+
+            # #make the api request
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                logger.error(f'API request failed: {e}')
+                return render(request, 'weather/weather.html', {'form': form, 'error': 'Failed to fetch weather data, Please try again later'})
+
+            weather_data = response.json()
+            # extract relevant data
+            weather = {
+                'city': weather_data.get('name'),
+                'temperature': weather_data.get('main', {}).get('temp'),
+                'description': weather_data.get('weather', [{}])[0].get('description'),
+                'icon': weather_data.get('weather', [{}])[0].get('icon'),
+            }
+            return render(request, 'weather/weather.html', {'form': form, 'weather': weather})
+        else:
+            return render(request, 'weather/weather.html', {'form': form})
     else:
-        return JsonResponse({'error': 'Please provide a city, lat/lon, or zip code.'}, status=400)
-
-    #make the api request
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        logger.error(f'API request failed: {e}')
-        return JsonResponse({'error': 'Failed to fetch weather data. Please try again later.'}, status=500)
-
-    weather_data = response.json()
-    # extract relevant data
-    data = {
-        'city': weather_data.get('name'),
-        'temperature': weather_data.get('main', {}).get('temp'),
-        'description': weather_data.get('weather', [{}])[0].get('description'),
-        'icon': weather_data.get('weather', [{}])[0].get('icon'),
-    }
-    return JsonResponse(data)
+        form = WeatherForm()
+        return render(request, 'weather/weather.html', {'form': form})
