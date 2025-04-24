d392a751914262645559c8f04e2d5bc2fa9f5c65
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-04-24 22:16:50 +0100

    Add user registration with custom user model

--- django-blog/blog_project/settings.py
+++ django-blog/blog_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'blog',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# new changes
+AUTH_USER_MODEL = 'accounts.CustomUser'
