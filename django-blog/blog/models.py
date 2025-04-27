b4c4c68dff595ce536b0f0856701df4c1c867600
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-04-27 19:59:00 +0100

    created post model and completed major pathof OAuth

--- django-blog/blog/models.py
+++ django-blog/blog/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
-# 🔐 User Authentication & Management
-# User registration
-# User login/logout
+# 🔐 User Authentication & Management (done)
+# User registration (done)
+# User login/logout (done)
 # Password reset via email
 # User profile with bio, picture, etc.
 # Admin-only dashboard for managing users and posts
--- django-blog/blog_project/settings.py
+++ django-blog/blog_project/settings.py
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # new changes
 AUTH_USER_MODEL = 'accounts.CustomUser'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/account/login/'
+LOGIN_REDIRECT_URL = '/accounts/home/'
+LOGOUT_REDIRECT_URL = '/accounts/logout/'
