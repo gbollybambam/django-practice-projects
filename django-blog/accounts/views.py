4837877e8cee5f8553eb7f7ec1ed0d23ba924b57
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-04-30 16:33:15 +0100

    upadate template for pagination and filtering of posts

--- django-blog/accounts/views.py
+++ django-blog/accounts/views.py
@@ -5,7 +5,10 @@ from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from blog.models import Post
+from django.core.paginator import Paginator
 import requests
+from random import shuffle
+
 
 # Create your views here.
 class UserRegisterView(View):
@@ -21,27 +24,43 @@ class UserRegisterView(View):
             return redirect('blog')
         return render(request, 'registration/signup.html', {'form': form})
 
-@login_required
-def Blog(request):
-    user_posts = Post.objects.all().order_by('-published_at')
-    api_key = settings.NEWS_API_KEY
-    url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + api_key
 
+def get_api_posts():
     try:
-        response = requests.get(url)
+        api_key = settings.NEWS_API_KEY
+        url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + api_key
+        response = requests.get(url, timeout=5)
         response.raise_for_status()
         api_data = response.json()
-        api_posts = api_data.get('articles', [])
-    except Exception as e:
+        return api_data.get('articles', [])
+    except requests.exceptions.RequestException as e:
         print(f'Error fetching external posts : {e}')
-        api_posts = []
+        return []
 
-    context = {
-        'user_posts': user_posts,
-        'api_posts': api_posts,
-    }
 
-    return render(request, 'blog/blog.html', context)
+@login_required
+def Blog(request):
+    filter_type = request.GET.get('source', 'all')
+    user_posts = Post.objects.all().order_by('-published_at')
+    api_posts = get_api_posts()
+
+    if filter_type == 'user':
+        combined = list(user_posts)
+    elif filter_type == 'api':
+        combined = api_posts
+    else:
+        combined = list(user_posts) + api_posts
+        shuffle(combined)
+
+
+    paginator = Paginator(combined, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/blog.html', {
+        'page_obj': page_obj,
+        'filter_type': filter_type
+    })
 
 # api key 
 # GET https://newsapi.org/v2/everything?q=Apple&from=2025-04-28&sortBy=popularity&apiKey=API_KEY
