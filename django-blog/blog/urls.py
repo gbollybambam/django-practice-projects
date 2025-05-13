386f959454060864b26ec2603fe21d633c94fcbc
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-05-13 22:22:48 +0100

    updating url

--- django-blog/blog/urls.py
+++ django-blog/blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api-post/<int:index>', api_post_detail, name='api_post_detail'),
     path('create/', PostCreateView.as_view(), name='create_post'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='edit_post'),
-    path('post/<int:pk>/delete/'. PostDeleteView.as_view(), name='delete_post')
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete_post')
 ]
