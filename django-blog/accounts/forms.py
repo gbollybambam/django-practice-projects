a9f1f37c06e3e73348416bf4004d9c08c0536ebe
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-05-19 22:16:17 +0100

    styling the form part of the login page

--- django-blog/accounts/forms.py
+++ django-blog/accounts/forms.py
@@ -5,8 +5,14 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 CustomUser = get_user_model()
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'autofocus': True}))
+    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={
+        'autofocus': True,
+        'class': 'form-control'
+        }))
 
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }))
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
