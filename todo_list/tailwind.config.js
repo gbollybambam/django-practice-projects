78847f2994d2cacdf56f3a6d096061506c17eb2c
Gbolahan olanrewaju <olanrewajugbolahan226@gmail.com>
2025-05-28 21:12:40 +0100

    ok

---
+++ todo_list/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './templates/**/*.html',
+      './**/templates/**/*.html',
+      './static/**/*.js',
+      // add other paths if needed
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
